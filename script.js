b40d7c5ba0fbf2eab54acccc22899ba6c42348c9
Talawa Life <76500833@protonmail.com>
2023-11-12 22:52:25 -0500

    fin

--- script.js
+++ script.js
@@ -3,16 +3,51 @@
 var generateBtn = document.querySelector("#generate");
 
 //! Defining generate password.
-function generatePassword(){
+
+function generatePassword() {
+  var numOfChars = prompt("How many characters would you like your password to be?", "");
   
-
-
-
-
-
-  return "Generated password wil go here."
-
+  if (numOfChars > 128) {
+    return "This is too many characters";
+  } else if (numOfChars < 8) {
+    return "This isn't enough characters";
+  }
+  //? The functions that return a random charactor are from https://webdesign.tutsplus.com/build-a-configurable-random-password-generator-with-javascript--cms-93262t
+  function getRandomLower() {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+  }
+  function getRandomNumber() {
+    return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+  }
+  function getSpecialChars() {
+    const symbols = "!@#$%^&*(){}[]=<>/,.";
+    return symbols[Math.floor(Math.random() * symbols.length)];
+  }
+  function getRandomUpper() {
+    return String.fromCharCode(Math.floor(Math.random() * 36) + 97);
+  }
+  var lower = window.confirm("Do you want lower case letters?");
+  var upper = window.confirm("Do you want to use upper case letters?");
+  var numbers = window.confirm("Do you want to use numbers?");
+  var specialChars = window.confirm("Do you want to use special characters?");
+  
+  var password = "";
+  //? This code is from https://webdesign.tutsplus.com/build-a-configurable-random-password-generator-with-javascript--cms-93262t
+  for (let i = 0; i < numOfChars; i += 1) {
+    if (lower) {
+      password += getRandomLower();
+    }
+    if (upper) {
+      password += getRandomUpper();
+    }
+    if (specialChars) {
+      password += getSpecialChars();
+    }
+  }
+  
+  return password;
 }
+
 // Write password to the #password input
 function writePassword() {
    //? 1.) generate password is being called
